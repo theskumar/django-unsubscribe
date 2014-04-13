2ca02d4248e6cbaf967760caa869140e063a864c
Saurabh Kumar <thes.kumar@gmail.com>
2014-04-13 21:40:25 +0530

    fix(test): make test pass for django 1.4

--- unsubscribe/runtests/settings.py
+++ unsubscribe/runtests/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'unsubscribe',
+    'unsubscribe.tests',
 )
 
 MIDDLEWARE_CLASSES = (
